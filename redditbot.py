001c5914d893da997bbaf2da56f8ae12c8898fc9
Semih Aslan <110099203+andmydignity@users.noreply.github.com>
2022-08-14 14:27:38 +0000

    Update redditbot.py

--- redditbot.py
+++ redditbot.py
@@ -24,28 +24,31 @@ reddit = praw.Reddit(
 subl=["Turkey","TurkeyJerky","ShitpostTC"]#Subs that bot will reply on
 d=0
 while True:
-    sub=reddit.subreddit(subl[d])
-    print("Searching in:"+subl[d])
-    for post in sub.new(limit=10):
-        if post.id in postid:
-            continue
-        for x in wordl:
-            if x in post.title.lower():
-                postid.append(post.id)
-                pic=sample(atal, 1)[0]
-                qou=sample(rtel, 1)[0]
-                rep_temp="Başlıkta RTE ile ilgili şeyler geçtiği için gerçek bir liderin [fotoğrafını]({}) paylaşmaya geldim"+"\n\n"+"\n\n"+"^(I am a bot and this action was performed automatically.)"+"\n\n"+"[Kaynak Kodu|Source Code](https://github.com/andmydignity/anti-tayyip-bot)"+"\n\n"+"^('{}' -R.T.E)"
-                rep=rep_temp.format(pic,qou)
-                post.reply(rep)
-                print("Replied to a post.({}|{})".format(subl[d],post.title))
-                break
+    try:
+        sub=reddit.subreddit(subl[d])
+        print("Searching in:"+subl[d])
+        for post in sub.new(limit=10):
+            if post.id in postid:
+                continue
+            for x in wordl:
+                if x in post.title.lower():
+                    postid.append(post.id)
+                    pic=sample(atal, 1)[0]
+                    qou=sample(rtel, 1)[0]
+                    rep_temp="Başlıkta RTE ile ilgili şeyler geçtiği için gerçek bir liderin [fotoğrafını]({}) paylaşmaya geldim"+"\n\n"+"\n\n"+"^(I am a bot and this action was performed automatically.)"+"\n\n"+"[Kaynak Kodu|Source Code](https://github.com/andmydignity/anti-tayyip-bot)"+"\n\n"+"^('{}' -R.T.E)"
+                    rep=rep_temp.format(pic,qou)
+                    post.reply(rep)
+                    print("Replied to a post.({}|{})".format(subl[d],post.title))
+                    break
 
-    if d==len(subl)-1:
-            d=0
-    else:
-        d+=1
-    if len(postid)>30:
-        for l in range(4):
-            postid.pop(x)
-    #Switch to an another sub
-    sleep(wait)
+        if d==len(subl)-1:
+                d=0
+        else:
+            d+=1
+        if len(postid)>30:
+            for l in range(4):
+                postid.pop(x)
+        #Switch to an another sub
+        sleep(wait)
+    except:
+        continue
