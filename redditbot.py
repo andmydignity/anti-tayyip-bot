a1fb00843b1501d1a37ff9800ed23d24f37a4463
Semih Aslan <110099203+andmydignity@users.noreply.github.com>
2022-09-27 16:09:33 +0000

    Fixed false positive issue*
     with false negative issue (but it's so much rarer)

--- redditbot.py
+++ redditbot.py
@@ -2,23 +2,19 @@ import praw
 from random import sample
 from time import sleep
 ata=open("atatürk.txt","r")
-rte=open("rte.txt","r")
 wait=15#Interval of requesting a call
 atal=[]#Atatürk picture links
-rtel=[]#RTE qoutes
 postid=[]
 for x in ata:
     atal.append(x)
-for x in rte:
-    rtel.append(x)
 wordl=["tayyip","erdoğan","teyyip","tahsin","rte","r.t.e","r.t.e.","uzun adam"]#Words that should be in title to bot to reply
 reddit = praw.Reddit(
-    client_id="",
-    client_secret="",
-    user_agent="",
-    username="",
-    password="",
-    ratelimit_seconds=1200,
+client_id="",
+client_secret="",
+user_agent="<console:LOL:1.0>",
+username="",
+password="",
+ratelimit_seconds=1200,
 )
 
 subl=["Turkey","TurkeyJerky","ShitpostTC"]#Subs that bot will reply on
@@ -31,15 +27,20 @@ while True:
             if post.id in postid:
                 continue
             for x in wordl:
-                if x in post.title.lower():
-                    postid.append(post.id)
-                    pic=sample(atal, 1)[0]
-                    qou=sample(rtel, 1)[0]
-                    rep_temp="Başlıkta RTE ile ilgili şeyler geçtiği için gerçek bir liderin [fotoğrafını]({}) paylaşmaya geldim"+"\n\n"+"\n\n"+"^(I am a bot and this action was performed automatically.)"+"\n\n"+"[Kaynak Kodu|Source Code](https://github.com/andmydignity/anti-tayyip-bot)"+"\n\n"+"^('{}' -R.T.E)"
-                    rep=rep_temp.format(pic,qou)
-                    post.reply(rep)
-                    print("Replied to a post.({}|{})".format(subl[d],post.title))
-                    break
+                k=post.title.lower()
+                i=k.find(x)
+                if i != -1:
+                    t1=k[i-1]
+                    u2=len(x)
+                    t2=k[i+u2]
+                    if t1 and t2 == " ":
+                        postid.append(post.id)
+                        pic=sample(atal, 1)[0]
+                        rep_temp="Başlıkta RTE ile ilgili şeyler geçtiği için gerçek bir liderin [fotoğrafını]({}) paylaşmaya geldim"+"\n\n"+"\n\n"+"^(I am a bot and this action was performed automatically.)"+"\n\n"+"[Kaynak Kodu|Source Code](https://github.com/andmydignity/anti-tayyip-bot)"
+                        rep=rep_temp.format(pic)
+                        post.reply(rep)
+                        print("Replied to a post.({}|{})".format(subl[d],post.title))
+                        break
 
         if d==len(subl)-1:
                 d=0
